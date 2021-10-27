8e240acb1c08be8ee271bae6e34cbc4295266dbb
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-27 21:56:57 +0200

    Functions

--- Function1.cs
+++ Function1.cs
@@ -25,9 +25,7 @@ namespace FunctionApp48
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = "";
 
             return new OkObjectResult(responseMessage);
         }
